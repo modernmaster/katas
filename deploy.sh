9f540e07770fb1cbd3ba835d345880e37de1bd45
James McGuigan <diesel_baz@hotmail.com>
2016-08-06 15:49:14 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-git checkout develop
-mvn release:prepare -B -Dskiptests
-mvn release:perform --settings deploy-settings.xml
+
+COMMITMESSAGE=$(git log --pretty=%s | head -1 | grep -G "^\[maven-release-plugin\] prepare for next development iteration$")
+if [ -z "$COMMITMESSAGE" ]; then 
+  git checkout develop
+  mvn release:prepare -B -Dskiptests
+  mvn release:perform --settings deploy-settings.xml
+fi
