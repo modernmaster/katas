71a2611e287422c9c9c3c49bbd26150b91295cad
James McGuigan <diesel_baz@hotmail.com>
2016-08-06 15:20:59 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-COMMITMESSAGE=$(git log --pretty=%s | head -1 | grep -G "^[maven-release-plugin] prepare for next development iteration$")
+COMMITMESSAGE=$(git log --pretty=%s | head -1 | grep -G "^\[maven-release-plugin\] prepare for next development iteration$")
 if [ -z "$COMMITMESSAGE" ]; then 
 	mvn verify 
 fi
