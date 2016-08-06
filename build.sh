bec94e47619048143ae4a29c5550d8678ec82cce
James McGuigan <diesel_baz@hotmail.com>
2016-08-06 14:49:59 +0100

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+COMMITMESSAGE=$(git log --pretty=%s | head -1 | grep -G "^[maven-release-plugin] prepare for next development iteration$")
+if [ -z "$COMMITMESSAGE" ]; then mvn verify fi
