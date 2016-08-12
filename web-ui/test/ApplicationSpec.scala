d1ab91d6c5fc1dc6452fe55f008f92958df61c6a
modernmaster <james.mcguigan@moneysupermarket.com>
2016-08-12 18:06:32 +0100

    #77-Install Play Framework Hello World App

---
+++ web-ui/test/ApplicationSpec.scala
@@ -0,0 +1,29 @@
+import org.junit.runner._
+import org.specs2.mutable._
+import org.specs2.runner._
+
+/**
+  * Add your spec here.
+  * You can mock out a whole application including requests, plugins etc.
+  * For more information, consult the wiki.
+  */
+@RunWith(classOf[JUnitRunner])
+class ApplicationSpec extends Specification //{
+
+//  "Application" should {
+
+    // always routes to home page
+    //    "send 404 on a bad request" in new WithApplication{
+    //      route(FakeRequest(GET, "/boum")) must beNone
+    //    }
+
+//    "render the index page" in new WithApplication {
+//
+//      val request = route(FakeRequest(GET, "/"))
+//      val home = request.get
+//      status(home) must equalTo(OK)
+//      contentType(home) must beSome.which(_ == "text/html")
+//      contentAsString(home) must contain("play-scala-angularjs-gulp")
+//    }
+//  }
+//}
