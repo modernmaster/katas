91f3de3627fe600755bcfd0ec0154a1ab62d6e53
jamesmcguigan <jamesmcguigan@Mac180anx.local>
2018-11-19 15:08:17 +0800

    98-Powerhands first commit

---
+++ powerhands/src/main/java/pokergame/Poker.java
@@ -0,0 +1,31 @@
+package pokergame;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Poker {
+
+  public List<Card> getCards() {
+    return cards;
+  }
+
+  private List<Card> cards;
+
+  public Poker(List<Card> cards) {
+    this.cards = cards;
+  }
+
+  public List<Card> dealCards() {
+    shuffleCards();
+    return pickFiveCards();
+  }
+
+  private List<Card> pickFiveCards() {
+    return cards.subList(0, 5);
+  }
+
+  private void shuffleCards() {
+    Collections.shuffle(cards);
+  }
+
+}
